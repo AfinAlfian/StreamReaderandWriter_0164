5ecdc553917056737a66ce8c3d26e4b117b4d23c
AfinAlfian <alfinalfiansyah528@gmail.com>
2024-06-04 10:55:10 +0700

    try

---
+++ ManualExeption/ManualExeption.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	try
+	{
+		cout << "Selamat Belajar di Prodi TI UMY" << endl;
+		//throw 0.5; //melemparkan sebuah integer maka 
+		cout << "Pernyataan tidak akan dieksekusi" << endl;
+	}
+	catch (const std::exception&)
+	{
+
+	}
+}
+
